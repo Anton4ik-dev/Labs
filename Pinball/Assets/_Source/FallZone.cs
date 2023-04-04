3ec2516fea337a8edcd0af8920246587ea8df0fb
Anton4ik-dev <afedot2004@gmail.com>
2023-04-04 13:58:50 +0300

    Fixed pinball

--- Pinball/Assets/_Source/FallZone.cs
+++ Pinball/Assets/_Source/FallZone.cs
@@ -1,21 +1,28 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FallZone : MonoBehaviour
 {
     [SerializeField] private GameObject ball;
     [SerializeField] private Transform spawnPos;
+    [SerializeField] private Text ballsText;
     [SerializeField] private int howMuchBalls;
 
-    private void OnCollisionEnter(Collision collision)
+    private void Start()
     {
-        if (collision.gameObject.CompareTag("Ball"))
+        ballsText.text = $"Balls: {howMuchBalls}";
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Ball"))
         {
             if (howMuchBalls > 0)
             {
                 Instantiate(ball, spawnPos);
                 Score.OnFall();
+                ballsText.text = $"Balls: {--howMuchBalls}";
             }
-            howMuchBalls--;
         }
     }
 }
